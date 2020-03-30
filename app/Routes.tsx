0151804a3b86c7145c92c0c25483cc9dfaeace17
Jaganathan, Bantheswaran <jbanthes@itron.com>
2020-03-30 19:45:49 +0530

    Removed container with help of hooks

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -3,14 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import routes from './constants/routes.json';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import Counter from './components/Counter';
 
 export default function Routes() {
   return (
     <App>
       <BrowserRouter>
         <Switch>
-          <Route path={routes.COUNTER} component={CounterPage} />
+          <Route path={routes.COUNTER} component={Counter} />
           <Route path={routes.HOME} component={HomePage} />
         </Switch>
       </BrowserRouter>
